6613a0394f7111b8e9f6131b9c6835244f2489c3
George Ciubotaru <geeku96@gmail.com>
2021-09-16 17:33:39 +0300

    feat: custom element

--- src/components/html/customElement.svelte
+++ src/components/html/customElement.svelte
@@ -1,32 +1,11 @@
 <script>
+    import * as elements from "svelte-elements/src";
     export let tag;
     export let className = undefined;
 
-    let content;
-    function uplift(node, _) {
-        // content will only be defined after the first render, so all logic can be done in update
-        return {
-            update({ tag, content, className }) {
-                const el = document.createElement(`${tag}`);
-                el.appendChild(content);
-                if (className) {
-                    el.classList.add(className);
-                }
-                node.appendChild(el);
-            },
-        };
-    }
+    let component = elements[tag];
 </script>
 
-<style>
-    div {
-        /* This removes the divs from the flow, so they do not have any effect on the layout */
-        display: contents;
-    }
-</style>
-
-<div use:uplift={{ content, tag, className }}>
-    <div bind:this={content}>
-        <slot />
-    </div>
-</div>
+<svelte this={component} class={className}>
+    <slot />
+</svelte>
